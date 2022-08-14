cd88331fa7fb94ba7cb38e7fe69c0f247d4f9afe
Ignacio Corball <ignaciocorball@techcoy.xyz>
2022-08-13 20:45:50 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { ExpensesChartComponentComponent } from './junior/expenses-chart-compone
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/junior/interactive-card-details-form/interactive-card-details-form.component.ts
+++ src/app/junior/interactive-card-details-form/interactive-card-details-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-interactive-card-details-form',
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InteractiveCardDetailsFormComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('form') formElement!: ElementRef;
+  @ViewChild('sucess') sucessElement!: ElementRef;
 
-  ngOnInit(): void {
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('cardNumberField') cardNumberField!: ElementRef;
+  @ViewChild('cvcField') cardCvcField!: ElementRef;
+  @ViewChild('expiryMonth') expiryMonthField!: ElementRef;
+  @ViewChild('expiryYear') expiryYearField!: ElementRef;
+  
+  submitted = false;
+  name: string = '';
+  cardNumber: string = '';
+  cvc: string = '';
+  expiryMonth: string = '';
+  expiryYear: string = '';
+
+  constructor( ) { }
+
+  ngOnInit(): void {  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.name != '' && this.cardNumber != '' && this.cvc != '' && this.expiryMonth != '' && this.expiryYear != ''){
+      this.formElement.nativeElement.style.display = 'none';
+      this.sucessElement.nativeElement.style.display = 'block';
+    }else{
+      this.nameField.nativeElement.focus();
+    }
   }
+
+  onReset(){
+    this.submitted = false;
+    this.formElement.nativeElement.style.display = 'block';
+    this.sucessElement.nativeElement.style.display = 'none';
+  }
+
+
+  onKeyPressCardholderName(event: any) {
+    this.name = event.target.value;
+  }
+  onKeyPressCardNumber(event: any) {
+    this.cardNumber = this.creditCardParse(event.target.value);
+    this.cardNumberField.nativeElement.value = this.cardNumber;
+  }
+  onKeyPressCardCvv(event: any) {
+    this.cvc = event.target.value;
+  }
+  onKeyPressExpiryMonth(event: any) {
+    this.expiryMonth = event.target.value;
+  }
+  onKeyPressExpiryYear(event: any) {
+    this.expiryYear = event.target.value;
+  }
+  
+  creditCardParse(value: any) {
+    return value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+  }
+    
 
 }
